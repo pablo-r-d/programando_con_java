9eab5076262cc1b4c5ad2db07c822b5792912425
pablo-r-d <pruidom319@g.educaand.es>
2023-11-08 20:07:24 +0100

    He puesto G como constante en lugar de usar 9.81 directamente en la ecuación en el ejercicio 6

--- 04_Sentencia_Condicional/ejercicios/T04Ejercicio06.java
+++ 04_Sentencia_Condicional/ejercicios/T04Ejercicio06.java
@@ -8,11 +8,11 @@ public class T04Ejercicio06 {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in, "Windows-1252");
-    
+    final double G = 9.81;
     System.out.print("Introduzca una altura (m) para calcular cuánto tardaría un objeto en caer desde la misma: ");
     double h = sc.nextDouble();
 
-    double t = Math.pow(2*h/9.81, 0.5);
+    double t = Math.pow(2*h/G, 0.5);
 
     if (h <= 0) {
       System.out.println("Ha introducido una altura no válida");
