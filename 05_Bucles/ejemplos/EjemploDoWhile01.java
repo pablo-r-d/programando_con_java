4f4e3044d2ecb710b0e1a83d84d7c0ce000c6114
pablo-r-d <pruidom319@g.educaand.es>
2023-11-14 17:15:03 +0100

    Actualizo los ejemplos de bucles

---
+++ 05_Bucles/ejemplos/EjemploDoWhile01.java
@@ -0,0 +1,19 @@
+/** 
+ * Explica tu código aquí
+ * 
+* @author Pablo Ruiz
+*/
+public class EjemploDoWhile01 {
+
+  public static void main(String[] args) {
+
+    int i = 1;
+    
+    do {
+      System.out.println(i);
+      i++;
+    } while (i < 11);
+
+  }
+
+}
---
+++ 05_Bucles/ejemplos/EjemploDoWhile02.java
@@ -0,0 +1,23 @@
+/** 
+ * Programa que lee  números de teclado mientras el número introducido sea par
+ * 
+* @author Pablo Ruiz
+*/
+import java.util.Scanner;
+public class EjemploDoWhile02 {
+
+  public static void main(String[] args) {
+    int num = 0;
+
+    do {
+      System.out.print("Introduce un número par: ");
+      num = Integer.parseInt(System.console().readLine());
+
+      if (num % 2 == 0) {
+        System.out.println("Ay qué bonito es el número par" + num);
+      }else{
+        System.out.println("No me gustan los números impares. Adiós.");
+      }
+    } while (num % 2 == 0);
+  }
+}
